7934a6d3fe1bb1c8bbd98a5cebb0d274f7dbdc96
Jeff Pinckney <jeffpinckney.90@gmail.com>
2018-12-07 10:11:33 -0700

    initial setup

---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+return(
+  <div>
+    <h1>This is my Header</h1>
+  </div>
+)
+}
---
+++ src/components/house/House.js
@@ -0,0 +1,8 @@
+export default function House(){
+  return (
+    <div>
+      <h1>This is my House</h1>
+    </div>
+  )
+
+}
