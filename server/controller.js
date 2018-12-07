eb129c3047649140e67afdf26b426c96300c8373
Jeff Pinckney <jeffpinckney.90@gmail.com>
2018-12-07 16:54:23 -0700

    last commit

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,17 @@
 module.exports = {
+
   getHouses: (req, res) => {
-    
+    const dbInstance = req.app.get('db')
+    const { houses } = req.body
+    dbInstance.getHouses([25])
+      .then((houses) => res.sendStatus(200))
+      .catch(error => {
+        res.status(500).send({ errorMessage: "Sorry no houses!" })
+        console.log(error)
+      })
   }
+
+
+
+
 }
--- server/index.js
+++ server/index.js
@@ -5,15 +5,15 @@ const massive = require('massive')
 const { SERVER_PORT } = process.env
 const { CONNECTION_STRING } = process.env
 // import {HashRouter} from 'react-router-dom'
+const app = express()
+app.use(express.json())
 
 /** This is connecting my app to the database */
 massive(CONNECTION_STRING)
+  .then(dbInstance => { app.set('db', dbInstance) })
+  .catch(error => console.log(error))
 
-const app = express()
-
-// app.get(`/${CONNECTION_STRING}`, controller.getHouses) -- revision or deletion
-
-app.get('')
+app.get('api/showHouses', controller.getHouses) 
 
 
 
